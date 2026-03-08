e9afbc5b4887b814356310196c9513559e29148c
Janet Spellman <janetspellman13@gmail.com>
2026-03-07 19:40:01 -0800

    bug: removing author name from discord embeds

--- themes/twentytwentyfive-child/functions.php
+++ themes/twentytwentyfive-child/functions.php
@@ -38,6 +38,13 @@ add_action( 'post_updated', function( $post_id ) {
     ) );
 }, 10, 1 );
 
+// removes page author information from discord embeds
+add_filter( 'oembed_response_data', 'disable_embeds_filter_oembed_response_data_' );
+function disable_embeds_filter_oembed_response_data_( $data ) {
+    unset($data['author_url']);
+    unset($data['author_name']);
+    return $data;
+}
 
 /*
   DESKTOP SUBMENU LOGIC (min-width: 601px)
