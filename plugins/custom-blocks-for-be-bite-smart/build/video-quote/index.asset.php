76dd66a10fd550203ffcda5b8440cf939aca4957
Janet Spellman <janetspellman13@gmail.com>
2026-03-12 01:05:25 -0700

    bug: accidently added fix to build file, added to src

--- plugins/custom-blocks-for-be-bite-smart/build/video-quote/index.asset.php
+++ plugins/custom-blocks-for-be-bite-smart/build/video-quote/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '72e12f553ebcfdfacbf4');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '6b6bdab73fa3e6f16213');
