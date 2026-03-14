cb0f4f07aa8f2844d608c3a2643d38e299d6256f
Janet Spellman <janetspellman13@gmail.com>
2026-03-14 15:14:20 -0700

    refactoring: had pdf-toggle block inherit from global button styles, article and commentary working with condensed style logic

--- plugins/custom-blocks-for-be-bite-smart/build/episode-card/index.asset.php
+++ plugins/custom-blocks-for-be-bite-smart/build/episode-card/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '27878d3ada3341088703');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd57f77e8b6ed934ed9e4');
--- plugins/custom-blocks-for-be-bite-smart/build/pdf-toggle/index.asset.php
+++ plugins/custom-blocks-for-be-bite-smart/build/pdf-toggle/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b28fb73d5e1680a8a226');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd512b9c289416f161813');
