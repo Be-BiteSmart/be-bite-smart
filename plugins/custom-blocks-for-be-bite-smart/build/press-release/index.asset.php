d62600946d2c960a2bc6048eeaf5b191fe414491
Janet Spellman <janetspellman13@gmail.com>
2026-03-14 18:57:46 -0700

    bug: view pdf for press release

--- plugins/custom-blocks-for-be-bite-smart/build/press-release/index.asset.php
+++ plugins/custom-blocks-for-be-bite-smart/build/press-release/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'f790b9f2fe9396c4c7a8');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '150ec65e8e67a358d443');
