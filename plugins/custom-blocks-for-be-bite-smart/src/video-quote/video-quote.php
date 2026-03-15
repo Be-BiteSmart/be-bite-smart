e6c0eaeadfc5078a953307dc643ec52c00a0d9be
Janet Spellman <janetspellman13@gmail.com>
2026-03-15 00:36:02 -0700

    refactor: improved video-quote.php size logic so it downloads the correct size image

--- plugins/custom-blocks-for-be-bite-smart/src/video-quote/video-quote.php
+++ plugins/custom-blocks-for-be-bite-smart/src/video-quote/video-quote.php
@@ -23,7 +23,7 @@ function render_video_quote_block( $attributes ) {
             'large',
             false,
             [
-                'sizes'   => '(max-width: 600px) 100vw, 800px',
+                'sizes' => '(min-width: 900px) 420px, 90vw',
                 'loading' => 'lazy',
                 'alt'     => wp_strip_all_tags( $title ),
             ]
