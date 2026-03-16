d38af9aaeda75e7c6c8dc5b9fb730ee55764e804
Janet Spellman <janetspellman13@gmail.com>
2026-03-15 21:38:46 -0700

    styling: added an arrow to make it clearer that the button sends them to another site, thats hosting the article

--- plugins/custom-blocks-for-be-bite-smart/build/news-and-coverage/index.asset.php
+++ plugins/custom-blocks-for-be-bite-smart/build/news-and-coverage/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f8e359207c4cf460d6df');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '430b884cad039d63bc9a');
